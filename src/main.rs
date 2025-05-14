8adb9427bf6cb8c19fcb4acf92c4b62932412d21
James Storr <jamesstorr@live.co.uk>
2025-05-14 07:23:52 +0100

    chapter 3.3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+#[tokio::main] //async runtime 
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        // handles all the transport layer concerns
+        App::new().route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
