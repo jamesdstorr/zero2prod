0024b31ddee59bfd7054d23c639f5179e124b558
James Storr <jamesstorr@live.co.uk>
2025-05-15 06:26:35 +0100

    3.5 Complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use actix_web::dev::Server;
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, HttpResponse, HttpServer, web};
+use std::net::TcpListener;
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         // handles all the transport layer concerns
         App::new().route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
